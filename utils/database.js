f6f5e2d09ffdbb44f2b7fc27ffe4190942aaec0f
Agung Jumantoro Andrian <53292050+Tomen1980@users.noreply.github.com>
2020-09-13 16:54:21 +0700

    Update database.js

--- utils/database.js
+++ utils/database.js
@@ -2,7 +2,7 @@ const Sequelize = require("sequelize");
 const session = require("express-session");
 const SessionStore = require("express-session-sequelize")(session.Store)
 const sequelize = new Sequelize(`${process.env.nameDB}`,`${process.env.userDB}`,`${process.env.passDB}`,{
-    dialect:`mysql`,
+    dialect:`${process.env.engineDB}`,
     host : `${process.env.hostDB}`,
     multipleStatements:true
 });
